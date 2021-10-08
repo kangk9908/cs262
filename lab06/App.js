071dab4d20af9567e712d166ce8858f9998aa274
Kun Kang <kunkang@dhcp86-229.calvin.edu>
2021-10-08 15:13:18 -0400

    lab06

---
+++ lab06/App.js
@@ -0,0 +1,54 @@
+/************************************************
+* App.js for cs262                              *
+* Lab06
+* Professor Keith VanderLinden                  *
+* Done by: Kun Kang                             *
+* 08 October 2021                               *
+*************************************************/
+import React, { Component } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      isLoading: true
+    };
+  }
+
+  async getBooks() {
+    try {
+      const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=jane%20austen');
+      const json = await response.json();
+      this.setState({ data: json.items });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+
+    return (
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text> {item.volumeInfo.title}, {item.volumeInfo.publishedDate}</Text>
+            )}
+          />
+        )}
+      </View>
+    );
+  }
+};
---
+++ lab06/README.md
@@ -0,0 +1,8 @@
+# CS 262 Lab06
+This lab is based on https://cs.calvin.edu/courses/cs/262/kvlinden/06hci/lab.html
+
+## Answers to the lab questions:
+A. What do the hooks in this code do?
+* The hooks in this code is the "setState" which lets us use react features without writing a class such as "state" and schedules an update to a component's state object.
+B. Modify the URL to some invalid value and explain how the code responds
+* When the URL is changed to some invalid value, the code responds by showing that there is no information to be fetched and displays nothing.
