01909ac8d7d5460702b1680b0d5634e41ccc62bc
Kun Kang <kunkang@Kuns-MacBook-Pro.local>
2021-10-17 20:14:28 -0400

    hw02

---
+++ hw02/App.js
@@ -0,0 +1,36 @@
+// **********************************************
+// * App.js for cs262-lab05                     *
+// * Professor Keith VanderLinden               *
+// * Done by: Kun Kang                          *
+// * 30 September 2021                          *
+// **********************************************
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PlayerScreen from "./screens/player";
+import DetailsScreen from "./screens/details";
+import AboutScreen from "./screens/about";
+import { globalStyles } from './styles/global';
+import { Button, View, Text, TouchableOpacity, FlatList, DrawerLayoutAndroidComponent, KeyboardAvoidingViewBase } from 'react-native';
+import React, { useState } from 'react';
+import Header from "./shared/header"
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Players" component={PlayerScreen} options={({ navigation }) => ({ headerRight: () => (
+        <Header navigation={navigation}/>
+    )
+})} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={({ navigation }) => ({ headerRight: () => (
+        <Header navigation={navigation}/>
+    )
+})} />
+        <Stack.Screen name="About" component={AboutScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
